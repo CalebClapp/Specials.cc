65dc08f8a091c3a7c89028677f41bdbdd4793f4f
Ryan Richardson <ryananthonyrichardson@gmail.com>
2014-02-13 00:17:32 -0800

    2 restaurant's specials added plus a bit more functionality

--- config.php
+++ config.php
@@ -1,7 +1,9 @@
 <?php
 
-  $json = json_decode(file_get_contents('deals.json'));
   date_default_timezone_set('America/Los_Angeles');
+  $json = json_decode(file_get_contents('deals.json'));
   $today = date('l');
+  $day_prefix = strtolower(substr($today,0,3));
+  $now = strtotime(strftime('%T'));
 
 ?>
--- index.php
+++ index.php
@@ -1,34 +1,64 @@
 <?php include('config.php'); ?>
-<!doctype html>
-<html>
+<!DOCTYPE html>
+<html class="no-js">
 <head>
-  <title><?php echo $today; ?> Food Deals</title>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>Bellingham Specials for <?php echo $today; ?></title>
+  <meta name="description" content="">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link href='http://fonts.googleapis.com/css?family=Architects+Daughter' rel='stylesheet' type='text/css'>
+  <link rel="stylesheet" href="css/main.css">
+  <script src="js/vendor/modernizr-2.7.1.min.js"></script>
 </head>
 <body>
+<div class="wrapper">
+  <div class="container m0a">
+    <header class="page-header">
+      <h1 class="tac">Bellingham Specials for <?php echo $today; ?></h1>
+    </header>
+      <?php 
+      $html = "<table class=\"entries m0a\">\n<tr>\n<th>Restaurant</th>\n
+        <th>Lunch Special</th>\n<th>Price</th>\n<th>Expires</th>\n</tr>";
+        foreach($json as $restaurant => $dishes) {
+          foreach($dishes as $dish => $meta) {
+            $start = new DateTime($meta->start);
+            $end = new DateTime($meta->end);
+            if (in_array($day_prefix, $meta->valid) &&
+               ($now >= strtotime($meta->start)) /*&&
+               ($now <= strtotime($meta->end))*/) { 
+                echo $html; ?>
 
-<table>
-  <th>
-    <?php echo $today; ?> Deals
-  </th>
-                                                <?php 
-                                                  foreach($json as $day => $entries) {
-                                                    if($day == strtolower($today)) {
-                                                      foreach($entries as $restaurant => $meta) { ?>
-    <tr>
-      <td class="entry-restaurant">
-        <?php echo $restaurant; ?>
-      </td>
-                                                      <?php 
-                                                        foreach($meta as $key => $value) { ?>
-      <td class="entry-<?php echo $key; ?>">
-        <?php echo $value; ?>
-      </td>
-                                                      <?php } ?>
-    </tr>
-                                                    <?php }
-                                                        }
-                                                      } 
-                                                    ?>
-</table>
+              <tr>
+                <td class="entry-restaurant">
+                  <?php echo $restaurant; ?>
+                </td>
+                <td class="entry-dish">
+                  <?php echo $dish; ?>
+                </td>
+              <?php 
+                foreach($meta as $key => $value) { 
+                  if(in_array($key, ['price','note'])) { ?>
+                    <td class="entry-<?php echo $key; ?>">
+                      <?php echo $value; ?>
+                    </td> <?php 
+                  } 
+                  if($key == 'end') { ?>
+                    <td class="entry-<?php echo $key; ?>">
+                      <?php echo date("H:i:s", (strtotime($value) - $now)); ?>
+                    </td> <?php 
+                  }  
+                } ?>
+              </tr> <?php 
+            } else { ?>
+              <h2 class="tac">No Deals Happening Right Now, Check Back Later.</h2> <?php 
+              break 2;
+            }
+          }
+        } ?>
+    </table>  
+  </div>
+</div>
+
 </body>
 </html>
